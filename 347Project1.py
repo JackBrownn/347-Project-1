6b21d2f44db37a0389e03107e8afb29bdc1ba20c
20njohnson <103784178+20njohnson@users.noreply.github.com>
2023-03-03 19:37:41 -0700

    Create 347Project1.py

--- 347Project1.py
+++ 347Project1.py
@@ -5,20 +5,20 @@ import matplotlib.pyplot as plt
 
 
 ###################### Mean #######################
-def mean(arr):
-    num_rows, num_cols = arr.shape
+def mean(arr): # function to calculate multivariate mean of dataset
+    num_rows, num_cols = arr.shape # stores number of rows and columns
     ret = []
     for i in range(num_cols):
         tot = 0
         for j in range(num_rows):
             tot = tot + arr[j,i]
-        ret.append(tot/num_rows)
+        ret.append(tot/num_rows) # appends column mean to dataset
     return(np.array(ret))
 
 
 ###################### Covariance #######################
-def covariance(v1,v2):
-    m1 = np.mean(v1)
+def covariance(v1,v2): # Function to find covariance of two vectors
+    m1 = np.mean(v1) 
     m2 = np.mean(v2)
     c1 = np.shape(v1)[0]
     ret = 0
@@ -27,27 +27,26 @@ def covariance(v1,v2):
     return (ret/(c1-1))
 
 ###################### Sample/Total Variance ##############
-def sampleVariance(arr):
-    arrayMean = np.mean(arr)
+def sampleVariance(arr): # Function to find sample variance of a vector
+    arrayMean = np.mean(arr) # Stores mean of array
     total = 0
     for i in range(arr.size):
         total += ((arr[i] - arrayMean) ** 2)
         
     return total / (arr.size - 1)
 
-def totalVariance(arr):
+def totalVariance(arr): # Function to find the total variance of a matrix
     cols = np.shape(arr)[1]
     totalVar = 0
     
     for column in range(cols):
-        print(sampleVariance(arr[:,column]))
-        totalVar += sampleVariance(arr[:,column])
+        totalVar += sampleVariance(arr[:,column]) # Adds sample variance to total variance
         
     return totalVar
 
 ###################### Negative Covariance #######################
-def negCovariance(arr):
-    cols = np.shape(arr)[1]
+def negCovariance(arr): # Function to find the number of negative covariances
+    cols = np.shape(arr)[1] # Stores number of columns in dataset
     num = 0
 
     for row in range(cols):
@@ -58,7 +57,7 @@ def negCovariance(arr):
     return num
 
 ###################### Correlation  #######################
-def correlation(v1,v2):
+def correlation(v1,v2): # Function to find the correlation between two vectors
     c1 = np.shape(v1)[0]
     m1 = 0
     m2 = 0
@@ -89,7 +88,7 @@ def correlation(v1,v2):
     return(top/(l1*l2))
 
 ###################### Correlation Greater #######################
-def correlationGreater(arr, g):
+def correlationGreater(arr, g): # Function to determine how many correlations are greater than a specific value
     cols = np.shape(arr)[1]
     num = 0
 
@@ -102,7 +101,7 @@ def correlationGreater(arr, g):
 
 
 ###################### Range Normalization #######################
-def rangeNormalization(arr, minimum, maximum):
+def rangeNormalization(arr, minimum, maximum): # Function using range normalization to normalize data
     difference = maximum - minimum
     rows = arr.shape[0]
     cols = arr.shape[1]
@@ -119,9 +118,9 @@ def rangeNormalization(arr, minimum, maximum):
 
 
 ###################### Standard Normalization #######################
-def standardNormalization(arr):
-    rows = arr.shape[0]
-    cols = arr.shape[1]
+def standardNormalization(arr): # Function using z-score normalization to normalize data
+    rows = arr.shape[0] # stores number of rows in data
+    cols = arr.shape[1] # stores number of columns in data
 
     retVal = np.eye(rows, cols)
     
@@ -135,23 +134,14 @@ def standardNormalization(arr):
 
 
 ###################### Covariance Matrix #######################
-def covarianceMatrix(arr):
-    cols = arr.shape[1]
-    covMat = np.empty([cols, cols])
-
 <<<<<<< HEAD
-    #if rowindex == columnindex set to variance (covariance(arrX, arrX))
-    #in covariance send (covariance(arr[row] and arr[column])
-    retVal = np.eye(rows, cols)
-    for indexRow in range(0, rows):
-        for indexColumn in range(0, cols):
-            retVal[indexRow][indexColumn] = covariance(arr[indexRow], arr[indexColumn])
-    return retVal
+def covarianceMatrix(arr): # Function to find the covariance matrix of a datset
+    cols = arr.shape[1]
+    covMat = np.empty([cols, cols]) # creates empty 2D array for covariance matrix
 
-=======
     for row in range(cols):
         for column in range(cols):
-            covMat[row, column] = covariance(arr[:,column], arr[:,row])
+            covMat[row, column] = covariance(arr[:,column], arr[:,row]) # stores covariance to covariance matrix
 
     return covMat
 =======
@@ -184,10 +174,9 @@ def covarianceMatrix(arr):
 #        for indexColumn in range(0, cols):
 #            retVal[indexRow][indexColumn] = covariance(arr[indexRow], arr[indexColumn])
 #    return retVal
->>>>>>> 558c2faa152d0cd346e2c2a79fbea566a22676d7
 
 ###################### Label Encoding #######################
-def labelEncoding(catArr):
+def labelEncoding(catArr): # Function using Label Encoding on categorical variables
   
     catArr = np.array(catArr)
     indArr = np.split(catArr, len(catArr))  #splits the 2D into individual 1D arrays
@@ -211,7 +200,7 @@ def labelEncoding(catArr):
 
 
 ###################### One Hot Encoding #######################
-def oneHotEncoding(catArr):
+def oneHotEncoding(catArr): # Function using One-Hot Encoding on categorical variable
 
     catArr = np.array(catArr)
     indArr = np.split(catArr, len(catArr))  #splits the 2D into individual 1D arrays
@@ -246,67 +235,22 @@ def oneHotEncoding(catArr):
 
 ############################################## Part 3 Answers ##########################################
 
+<<<<<<< HEAD
 #import os
 #print(os.getcwd())  
 from numpy import genfromtxt
 
-#read in data: must be in the 347-project-1 directory to work
 data =  pd.read_csv('adultTest.data', sep=",") 
 
-#label encode the categorical attributes and turn matrix in to numpy array
 categoricalAttributes = data.iloc[:, [1, 3, 5, 6, 7, 8, 9, 13, 14]]
-numericalAttributes = data.iloc[:, [2, 4, 10, 11, 12]]
-numericalAttributes = np.array(numericalAttributes)
-
-
 data.iloc[:, [1, 3, 5, 6, 7, 8, 9, 13, 14]] = labelEncoding(categoricalAttributes)
 data = np.array(data)
 
 
-
-
-#Print answers to Part 3
 print('Multivariate Mean: ', mean(data))
 print('Covariance Matrix: ', covarianceMatrix(data))
-print('Scatter Plots: ', )
+print('Scatter Plots: ')
 print('Multivariate Mean: ')
-<<<<<<< HEAD
-print("blah")
-
-#print(numericalAttribute2)
-
-
-
-zScore = standardNormalization(numericalAttributes)
-zScore2 = []
-zScore4 = []
-zScore10 = []
-zScore11 = []
-zScore12 = []
-for i in range(len(zScore)):
-    zScore2.append(zScore[i][0])
-    zScore4.append(zScore[i][1])
-    zScore10.append(zScore[i][2])
-    zScore11.append(zScore[i][3])
-    zScore12.append(zScore[i][4])
-
-
-print(covariance(zScore2, zScore4))
-print(covariance(zScore2, zScore10))
-print(covariance(zScore2, zScore11))
-print(covariance(zScore2, zScore12))
-
-print(covariance(zScore4, zScore10))
-print(covariance(zScore4, zScore11))
-print(covariance(zScore4, zScore12))
-
-print(covariance(zScore10, zScore11))
-print(covariance(zScore10, zScore12))
-
-print(covariance(zScore11, zScore12))
-
-
-=======
 =======
 #data = 
 >>>>>>> 589bb1a62c6bfd36439d99f61ffc37628339f84c
@@ -350,36 +294,3 @@ print(totalVariance(data))
 
 # TOTAL VARIANCE OF 5 GREATEST
 print(str(12089776858.609715 + 4105580.4150943398 + 173946.83018867925 + 26.41509433962264 + 0.7169811320754716))
->>>>>>> 558c2faa152d0cd346e2c2a79fbea566a22676d7
-
-
-
-
-##################
-zScore = standardNormalization(numericalAttributes)
-zScore2 = []
-zScore4 = []
-zScore10 = []
-zScore11 = []
-zScore12 = []
-for i in range(len(zScore)):
-    zScore2.append(zScore[i][0])
-    zScore4.append(zScore[i][1])
-    zScore10.append(zScore[i][2])
-    zScore11.append(zScore[i][3])
-    zScore12.append(zScore[i][4])
-
-
-print(covariance(zScore2, zScore4))
-print(covariance(zScore2, zScore10))
-print(covariance(zScore2, zScore11))
-print(covariance(zScore2, zScore12))
-
-print(covariance(zScore4, zScore10))
-print(covariance(zScore4, zScore11))
-print(covariance(zScore4, zScore12))
-
-print(covariance(zScore10, zScore11))
-print(covariance(zScore10, zScore12))
-
-print(covariance(zScore11, zScore12))
